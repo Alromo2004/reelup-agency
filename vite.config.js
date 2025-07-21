df79884c1423a3004c79e7b012ce4057c0b01b06
Alromo2004 <alromo2004@gmail.com>
2025-07-21 08:52:34 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: './', // 👈 Esto arregla el problema en producción
-})
+  plugins: [react()]
+});
